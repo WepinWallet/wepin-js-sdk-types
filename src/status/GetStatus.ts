5b0dc65c53c8d37cdba9d27e5fa089b87f77a68a
HyunjungKim <96401185+hyunjung222@users.noreply.github.com>
2023-08-18 07:10:13 +0000

    #3 - add IStatus type

---
+++ src/status/GetStatus.ts
@@ -0,0 +1,9 @@
+export interface IStatus {
+    login: boolean
+    loginStatus?: 'completed' | 'registerRequired'
+  }
+  
+  export type GetStatusFunction = (
+    option?: {waitForClose: boolean}
+  ) => Promise<IStatus | undefined>
+  
